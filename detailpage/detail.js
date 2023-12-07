30406346a594faaa2bb62cd40a8221f6c0c2ad97
Takuya Okada <s246048@wakayama-u.ac.jp>
2023-12-07 13:28:53 +0900

    create detailpage

---
+++ detailpage/detail.js
@@ -0,0 +1,36 @@
+// 予定の追加
+function addRow() {
+  // 表のtbody要素を取得
+  var tbody = document.getElementById('schedule').getElementsByTagName('tbody')[0];
+
+  // 新しい行を作成
+  var newRow = tbody.insertRow();
+
+  // 新しい行にセルを挿入
+  var timeCell = newRow.insertCell(0);
+  var eventCell = newRow.insertCell(1);
+  var memoCell = newRow.insertCell(2);
+
+  // セルにinput要素を追加
+  timeCell.innerHTML = '<input type="time">';
+  eventCell.innerHTML = '<input type="text" placeholder="イベントを入力">';
+  memoCell.innerHTML = '<input type="text" placeholder="備考欄">';
+}
+
+// 予定の削除
+function deleteRow() {
+  // 表のtbody要素を取得
+  var tbody = document.getElementById('schedule').getElementsByTagName('tbody')[0];
+
+  // tbody要素にある最後の行を削除
+  tbody.deleteRow(-1);
+}
+
+// 日程の追加(createPlan)作成中
+function adddDate(){
+  // 新たな日程を作成
+  var newDate = document.createElement('table');
+  newDate.setAttribute('id', 'Date');
+  
+  // ヘッダーを追加
+}
